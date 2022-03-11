ff8d67aaa37c434b0a378448787d8ab6f80b125d
yoon <yunyun40550082@gmail.com>
2022-03-12 03:15:34 +0900

    refactor: 옵션 1개 선택되도록 수정

--- src/components/SelectedOptions.js
+++ src/components/SelectedOptions.js
@@ -1,38 +1,41 @@
-import Component from './Component.js'
-import formatPriceAddComma from '../utils/formatPrice.js'
+import Component from './Component.js';
+import formatPriceAddComma from '../utils/formatPrice.js';
 
 export default class SelectedOptions extends Component {
-   
-  calculateTotalPrice(selectedOptionValues){
-    const priceArr = Object.values(selectedOptionValues).map(({optionTotalPrice}) => Number(optionTotalPrice))
-    return priceArr.reduce((a,b) => (Number(a)+Number(b)));
-  }
- 
-    template(){
-      const {productPrice, selectedOptions, selectedOptionsData} = this.props
-        return`
+  template() {
+    const { productPrice, selectedOption, selectedOptionData } = this.props;
+    const { optionId, optionName, optionPrice, optionStock } = selectedOption;
+    return `
         <h3>선택된 상품</h3>
         <ul>
-          ${selectedOptions.map(({optionId, optionName, optionPrice, optionStock})=>`
-              <li>
-              ${optionName} ${formatPriceAddComma(Number(productPrice)+Number(optionPrice))}원 
-                <div>
-                  <input data-option-id=${optionId} name='option-input' type="number", data-option-price=${optionPrice} value=${selectedOptionsData[optionId]?.count} min="1" max="${optionStock}">개
-                </div>
-              </li>
-          `).join('')}
+          ${
+            optionId &&
+            `
+          <li>
+          ${optionName} ${formatPriceAddComma(
+              Number(productPrice) + Number(optionPrice),
+            )}원 
+            <div>
+              <input data-option-id=${optionId} name='option-input' type="number", data-option-price=${optionPrice} value=${
+              selectedOptionData.count
+            } min="1" max="${optionStock}">개
+            </div>
+          </li>
+          `
+          }
         </ul>
-        <div class="ProductDetail__totalPrice">${Object.keys(selectedOptionsData).length ? this.calculateTotalPrice(selectedOptionsData) : '0'}</div>
+        <div class="ProductDetail__totalPrice">${
+          selectedOptionData.optionTotalPrice
+        }</div>
         <button class="OrderButton">주문하기</button>
-        `
-    }
+        `;
+  }
 
-    setEvent(){
-      const $inputList = this.$target.querySelectorAll('[name="option-input"]');
-      $inputList.length && Array.from($inputList).map(($input)=>{
-        $input.addEventListener('change', (e) => {
-          this.props.onChange(e);
-        })
-      })
-    }
+  setEvent() {
+    const $input = this.$target.querySelector('[name="option-input"]');
+    $input &&
+      $input.addEventListener('change', e => {
+        this.props.onChange(e);
+      });
+  }
 }
--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,105 +1,124 @@
-import Component from '../components/Component.js'
-import Select from '../components/Select.js'
-import SelectedOptions from '../components/SelectedOptions.js'
-import formatPriceAddComma from '../utils/formatPrice.js'
-import {request} from '../utils/api.js'
+import Component from '../components/Component.js';
+import Select from '../components/Select.js';
+import SelectedOptions from '../components/SelectedOptions.js';
+import formatPriceAddComma from '../utils/formatPrice.js';
+import { request } from '../utils/api.js';
 
 export default class ProductDetailPage extends Component {
-    async setup() {
-        const res = await request(`/products/${this.props.id}`);
-        this.setState({
-            productData:{
-                ...res,
-                productId:res.id,
-                productName:res.name,
-                productPrice:res.price,
-            }, 
-            selectedOptions:[], 
-            selectedOptionsData:{}, 
-        });
+  async setup() {
+    const res = await request(`/products/${this.props.id}`);
+    this.setState({
+      productData: {
+        ...res,
+        productId: res.id,
+        productName: res.name,
+        productPrice: res.price,
+      },
+      selectedOption: {
+        optionId: '',
+        optionStock: 0,
+        optionName: '',
+        optionPrice: 0,
+      },
+      selectedOptionData: {
+        optionId: '',
+        count: 0,
+        optionTotalPrice: 0,
+      },
+    });
+  }
+
+  calculateOptonTotalPrice(productPrice, optionPrice, count) {
+    return (Number(productPrice) + Number(optionPrice)) * Number(count);
+  }
+
+  handleChangeSelect(selectedOption) {
+    const { optionId, optionPrice } = selectedOption;
+    const { productPrice } = this.state.productData;
+
+    this.state.optionId !== optionId &&
+      this.setState({
+        selectedOption: selectedOption,
+        selectedOptionData: {
+          optionId,
+          count: 1,
+          optionTotalPrice: this.calculateOptonTotalPrice(
+            productPrice,
+            optionPrice,
+            1,
+          ),
+        },
+      });
+  }
+
+  handleChangeInput(e) {
+    const { value } = e.target;
+    const { optionId, optionPrice } = e.target.dataset;
+    const { productPrice } = this.state.productData;
+
+    this.setState({
+      selectedOptionData: {
+        optionId,
+        count: value,
+        optionTotalPrice: this.calculateOptonTotalPrice(
+          productPrice,
+          optionPrice,
+          value,
+        ),
+      },
+    });
+  }
+
+  handleSubmit() {
+    const { productId } = this.state.productData;
+  }
+
+  template() {
+    if (!this.state) {
+      return '';
     }
 
-    calculateOptonTotalPrice(productPrice, optionPrice, count) {
-        return  (Number(productPrice) + Number(optionPrice)) * (Number(count));
-      }
-
-    handleChangeSelect(selectedOption){
-        for(const {optionId} of this.state.selectedOptions) {
-            if(selectedOption.optionId === optionId) {
-                return
-            }
-        }
-
-        const {optionId, optionPrice} = selectedOption
-        const {productPrice} = this.state.productData;
-
-        this.setState({
-            selectedOptions:[...this.state.selectedOptions, selectedOption],
-            selectedOptionsData:{
-                ...this.state.selectedOptionsData, 
-                [optionId]:{
-                    count:1, 
-                    optionTotalPrice: this.calculateOptonTotalPrice(productPrice, optionPrice, 1)}
-            },  
-        });
-    }
-
-    handleChangeInput(e){
-        const {value} = e.target
-        const {optionId, optionPrice} = e.target.dataset
-        const {productPrice} = this.state.productData;
-
-        this.setState({
-            selectedOptionsData:{
-                ...this.state.selectedOptionsData, 
-                [optionId]:{
-                    count:value, 
-                    optionTotalPrice: this.calculateOptonTotalPrice(productPrice, optionPrice, value)}
-            }, 
-        })
-      }
-
-    handleSubmit(){
-        const {productId} = this.state.productData;
-    }  
-
-    template() {
-        if(!this.state) {
-            return ''
-        }
-
-        const {productName, imageUrl, productPrice} = this.state.productData;
-        return `
+    const { productName, imageUrl, productPrice } = this.state.productData;
+    return `
           <div class="ProductDetailPage">
             <h1>${productName} 상품 정보</h1>
             <div class="ProductDetail">
             <img src=${imageUrl}>
             <div class="ProductDetail__info">
               <h2>${productName} </h2>
-              <div class="ProductDetail__price">${formatPriceAddComma(productPrice)}</div>
+              <div class="ProductDetail__price">${formatPriceAddComma(
+                productPrice,
+              )}</div>
               <div class="ProductDetail__selectContainer"></div>
               <div class="ProductDetail__selectedOptions"></div>
             </div>
           </div>
         `;
+  }
+
+  mounted() {
+    if (!this.state) {
+      return;
     }
 
-    mounted() {
-        if(!this.state) {
-            return 
-        }
+    const $selectContainer = this.$target.querySelector(
+      '.ProductDetail__selectContainer',
+    );
+    const $selectedOptions = this.$target.querySelector(
+      '.ProductDetail__selectedOptions',
+    );
+    const { productData, selectedOption, selectedOptionData } = this.state;
+    const { productPrice } = productData;
 
-        const $selectContainer = this.$target.querySelector('.ProductDetail__selectContainer');
-        const $selectedOptions = this.$target.querySelector('.ProductDetail__selectedOptions');
-        const {productData, selectedOptions, selectedOptionsData} = this.state
-        const {productPrice} = productData
-
-        new Select($selectContainer, {productData, onChange:this.handleChangeSelect.bind(this)});
-        new SelectedOptions($selectedOptions, {
-            productPrice,
-            selectedOptions,
-            selectedOptionsData,
-            onChange:this.handleChangeInput.bind(this)
-        })
-    } 
+    new Select($selectContainer, {
+      productData,
+      onChange: this.handleChangeSelect.bind(this),
+    });
+    new SelectedOptions($selectedOptions, {
+      productPrice,
+      selectedOption,
+      selectedOptionData,
+      onChange: this.handleChangeInput.bind(this),
+    });
+  }
 }
